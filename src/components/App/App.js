363c31eb6d543789cc0c793144fa7eb9dec69d27
kyleteeter <kyleteeter12@gmail.com>
2022-04-04 16:48:00 -0500

    Add clear function to search bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import {
-  extendTheme,
-  ChakraProvider,
-  Box,
-} from '@chakra-ui/react';
+import React from "react";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
 import { useState } from "react";
-import { Header } from '../Header';
+import { Header } from "../Header";
 
 const layout = {
   width: {
-    60: '60%'
+    60: "60%",
   },
-}
+};
 
-const theme = extendTheme({ layout })
-
+const theme = extendTheme({ layout });
 
 export function App() {
-  const [gifs, setGifs] = useState([]);
+  const [gifs, setGifs] = useState({});
 
   const getGifs = (searchValue) => {
-    console.log('search', searchValue)
-    fetch(`https://api.giphy.com/v1/gifs/search?api_key=JVYjg9uDDbG7MnCkRHJkAFb2psQw5kDW&q=${searchValue}&limit=20`)
-    .then(response => response.json())
-    .then((response) => setGifs(response))
-    .then(console.log('gifs', gifs))
+    console.log("search", searchValue);
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=JVYjg9uDDbG7MnCkRHJkAFb2psQw5kDW&q=${searchValue}&limit=20`
+    )
+      .then((response) => response.json())
+      .then((data) => setGifs(data));
+  };
+
+  const clearResults = () => {
+    setGifs({});
   }
 
   return (
     <ChakraProvider theme={theme}>
-        <Header getGifs={getGifs} gifs={gifs} />
+      <Header getGifs={getGifs} gifs={gifs} clearResults={clearResults} />
     </ChakraProvider>
   );
 }
-
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,31 +5,35 @@ import {
   InputGroup,
   InputLeftElement,
   Flex,
+  FormControl,
+  InputRightElement,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "../ColorModeSwitcher";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaTimes } from "react-icons/fa";
 import { useState, useEffect } from "react";
 
-
-export function Header({ getGifs, gifs }) {
+export function Header({ getGifs, gifs, clearResults }) {
   const [inputValue, setInputValue] = useState("");
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
-    alert(inputValue)
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = (input) => {
+    getGifs(input);
+  };
+
+  const handleReset = (event) => {
     event.preventDefault();
-    getGifs(inputValue);
+    setInputValue("");
+    clearResults();
   };
 
   useEffect(() => {
     const listener = (event) => {
       if (event.code === "Enter" || event.code === "NumpadEnter") {
         event.preventDefault();
-        console.log("search", inputValue);
-        handleSubmit();
+        handleSubmit(inputValue);
       }
     };
     document.addEventListener("keydown", listener);
@@ -43,19 +47,31 @@ export function Header({ getGifs, gifs }) {
       <Text fontFamily={"heading"} fontSize='2xl'>
         Giphy App
       </Text>
-      <InputGroup maxW='60%'>
-        <InputLeftElement
-          pointerEvents='none'
-          children={<FaSearch color='gray.300' />}
-        />
-        <Input
-          type='text'
-          placeholder='Search Gifs'
-          value={inputValue}
-          onChange={handleChange}
-        />
-      </InputGroup>
+      <FormControl maxW='60%'>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents='none'
+            children={<FaSearch color='gray.300' />}
+          />
+          <Input
+            type='text'
+            placeholder='Search Gifs'
+            value={inputValue}
+            onChange={handleChange}
+          />
+          {inputValue.length ? (
+            <InputRightElement
+              children={<FaTimes color='gray.300' />}
+              onClick={handleReset}
+            ></InputRightElement>
+          ) : (
+            ""
+          )}
+        </InputGroup>
+      </FormControl>
       <ColorModeSwitcher maxW='35%' />
+      {console.log(gifs)}
+      {console.log("input", inputValue)}
     </Flex>
   );
 }
